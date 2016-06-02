a4310935b52e1edd72eb403a05f65e0dc7ec410b
ShehabCE <shehab_m@aucegypt.edu>
2016-06-02 19:49:36 +0200

    Done some switch cases and Operand Read functions based on OpCode read from Instruction. (Note: Re-check the operand variables in the second IM iteration.)

--- SIM Applitcation/SIM Applitcation/SIM_Microprocessor.cpp
+++ SIM Applitcation/SIM Applitcation/SIM_Microprocessor.cpp
@@ -1,5 +1,10 @@
 #include "SIM_Microprocessor.h"
 
+int Lookup(std::string);
+void ReadThreeOperands(std::string, int&, std::string&, std::string&, std::string&);
+void ReadTwoOperands(std::string, int&, std::string&, std::string&);
+void ReadOneOperand(std::string, int&, std::string&);
+
 SIM::SIM()
 {
 	//std::map <std::string, ADD*> HashTable;
@@ -58,12 +63,16 @@ void SIM::ReadFile()
 
 void SIM::Execute()
 {
+	int OPCODE;
 	int cursor = 0;	int SizeOfIM = count;
 	std::string CurrentInstruction;
-	std::string operation ="", operand1, operand2, operand3;
-	for (int j = 0; j < SizeOfIM; j++)
+	std::string operation;
+	std::string operand1, operand2, operand3;
+
+	for (int j = 0; j < SizeOfIM; j++)		//Looping through the Whole Instruction Memory till the end.
 	{
 		CurrentInstruction = SIM_IM->LoadFromInstructionMemory(j);
+		//This block of code is reading the operation type and stores it in operation variable.
 		while (1)
 		{
 			if (operation.empty())
@@ -72,22 +81,52 @@ void SIM::Execute()
 				operation += CurrentInstruction[cursor];
 			cursor++;
 			if (CurrentInstruction[cursor] == ' ')
+			{
+				cursor++;
 				break;
+			}
 		}
-		while (CurrentInstruction[cursor] != ',')
+		OPCODE = Lookup(operation);
+		//Calling a function to read the operands based on what type of instruction it is.
+		switch (OPCODE)
 		{
-			if (operand1.empty())
-				operand1 = CurrentInstruction[cursor];
-			else
-				operand1 += CurrentInstruction[cursor];
-			cursor++;
+
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			ReadThreeOperands(CurrentInstruction, cursor, operand1, operand2, operand3);
+			//Call 3 operands read function.
+			//To do.
+			break;
+		
+		case 7:
+		case 8:
+		case 9:
+			//Call 2 operands read function.
+			//To do.
+			break;
+
+		case 10:
+		case 11:
+		case 12:
+			//Call  operands read function.
+			//To do.
+			break;
+
+		case 13:
+			//Halt
+			break;
+		
+		default:
+			std::cerr << "Unidentified Instruction Type." << std::endl;
+			break;
 		}
-		std::cout << "Hello" << std::endl;
-		//Got an instruction. Check first word? If valid, read operands corresponding to instruction. (Handle errors).
-		//Each instruction will be instantiated once in the SIM constructor.
-		//Whenever there is a match, use Instruction.Execute() method.
-		//Else, print a nonsense statement. 
-		//Use unordered_map ??
+		// ADD A,B,0
+		//Example: operation = "ADD"	operand1 = "A"	operand2 = "B"	operand3 = "0"
+
 	}
 }
 
@@ -96,3 +135,150 @@ SIM::~SIM()
 	//delete SIM_IM, SIM_DM, InstructionCount;
 	//delete []InstrEntry;
 };
+
+int Lookup(std::string S)
+{
+	int OperationNumber;
+
+	//Start of Three Operand-Instructions.
+	if (S == "ADD")
+		OperationNumber = 1;
+	else if (S == "SUB")
+		OperationNumber = 2;
+	else if (S == "MUL")
+		OperationNumber = 3;
+	else if (S == "LESS")
+		OperationNumber = 4;
+	else if (S == "LARGER")
+		OperationNumber = 5;
+	else if (S == "EQUAL")
+		OperationNumber = 6;
+	//Start of Two Operand-Instructions.
+	else if (S == "NEG")
+		OperationNumber = 7;
+	else if (S == "JMP0")
+		OperationNumber = 8;
+	else if (S == "ASSIGN")
+		OperationNumber = 9;
+	//Start of One Operand-Instructions.
+	else if (S == "JMP")
+		OperationNumber = 10;
+	else if (S == "READ")
+		OperationNumber = 11;
+	else if (S == "WRITE")
+		OperationNumber = 12;
+	else return 13;		//HALT.
+
+}
+
+void ReadThreeOperands(std::string LINE, int& cursor, std::string& IN1, std::string& IN2, std::string& OUT)
+{
+	int CountCommas = 0, EndOfLine = LINE.length();
+
+	//Keep reading operands till hits end of instruction.
+
+	while (CountCommas < 1)		//IN1
+	{
+		if (LINE[cursor] == ' ')
+			continue;		//SKIP.
+
+		if (IN1.empty())
+			IN1 = LINE[cursor];
+		else
+			IN1 += LINE[cursor];
+		cursor++;
+		if (LINE[cursor] == ',')
+		{
+			CountCommas++;
+			cursor++;
+		}
+	}
+
+	while (true)		//IN2
+	{
+		if (LINE[cursor] == ' ')
+			continue;		//SKIP.
+
+		if (IN2.empty())
+			IN2 = LINE[cursor];
+		else
+			IN2 += LINE[cursor];
+		cursor++;
+		if (LINE[cursor] == ',')
+		{
+			CountCommas++;
+			cursor++;
+		}
+	}
+
+	while (CountCommas < 3)		//OUT
+	{
+		if (LINE[cursor] == ' ')
+			continue;		//SKIP.
+
+		if (OUT.empty())
+			OUT = LINE[cursor];
+		else
+			OUT += LINE[cursor];
+		cursor++;
+		if (cursor == EndOfLine)
+			break;
+	}
+}
+
+void ReadTwoOperands(std::string LINE, int& cursor, std::string& IN1, std::string& OUT)
+{
+	int CountCommas = 0, EndOfLine = LINE.length();
+
+	//Keep reading operands till hits end of instruction.
+
+	while (CountCommas < 1)		//IN1
+	{
+		if (LINE[cursor] == ' ')
+			continue;		//SKIP.
+
+		if (IN1.empty())
+			IN1 = LINE[cursor];
+		else
+			IN1 += LINE[cursor];
+		cursor++;
+		if (LINE[cursor] == ',')
+		{
+			CountCommas++;
+			cursor++;
+		}
+	}
+
+	while (true)		//OUT
+	{
+		if (LINE[cursor] == ' ')
+			continue;		//SKIP.
+
+		if (OUT.empty())
+			OUT = LINE[cursor];
+		else
+			OUT += LINE[cursor];
+		cursor++;
+		if (cursor == EndOfLine)
+			break;
+	}
+}
+
+void ReadOneOperand(std::string LINE, int& cursor, std::string& OUT)
+{
+	int EndOfLine = LINE.length();
+
+	while (true)		//OUT
+	{
+		if (LINE[cursor] == ' ')
+			continue;		//SKIP.
+
+		if (OUT.empty())
+			OUT = LINE[cursor];
+		else
+			OUT += LINE[cursor];
+		cursor++;
+		if (cursor == EndOfLine)
+			break;
+	}
+}
